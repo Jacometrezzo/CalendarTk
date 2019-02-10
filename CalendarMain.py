62253e9ff7bfd09b83a8156f471ac0d0f5a39482
Victor Fernandez Marcilla <VictorFernandezMarcilla@Mac-mini.home>
2019-02-10 11:14:25 +0100

    El calendario con numeros de colores

--- CalendarMain.py
+++ CalendarMain.py
@@ -3,6 +3,7 @@ import datetime
 from tkinter import *
 from tkinter import ttk
 
+
 class MainApp(Tk):
     
     def __init__(self):
@@ -35,6 +36,58 @@ class MainApp(Tk):
         canvas.create_line(0,304, 532,304, width = 1)
         canvas.create_line(0,365, 532,365, width = 1)
         canvas.create_line(0,426, 532,426, width = 1)
+
+        canvas.create_text(266,20, anchor=(CENTER), font=('Arial', '24', 'bold'), text='Febrero 2019')
+        canvas.create_text(38,50, anchor=(CENTER), font=('Arial', '11'), text='Lunes')
+        canvas.create_text(114,50, anchor=(CENTER), font=('Arial', '11'), text='Martes')
+        canvas.create_text(190,50, anchor=(CENTER), font=('Arial', '11'), text='Miércoles')
+        canvas.create_text(266,50, anchor=(CENTER), font=('Arial', '11'), text='Jueves')
+        canvas.create_text(342,50, anchor=(CENTER), font=('Arial', '11'), text='Viernes')
+        canvas.create_text(418,50, anchor=(CENTER), font=('Arial', '11'), text='Sábado')
+        canvas.create_text(494,50, anchor=(CENTER), font=('Arial', '11'), text='Domingo')
+
+        canvas.create_text(68,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='28')
+        canvas.create_text(144,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'),text='29')
+        canvas.create_text(220,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'),text='30')
+        canvas.create_text(296,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'),text='31')
+        canvas.create_text(372,113, anchor=('se'), font=('Arial', '28', 'bold'), text='1')
+        canvas.create_text(448,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'),text='2')
+        canvas.create_text(524,113, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'), text='3')
+        canvas.create_text(68,174, anchor=('se'), font=('Arial', '28', 'bold'), text='4')
+        canvas.create_text(144,174, anchor=('se'), font=('Arial', '28', 'bold'), text='5')
+        canvas.create_text(220,174, anchor=('se'), font=('Arial', '28', 'bold'), text='6')
+        canvas.create_text(296,174, anchor=('se'), font=('Arial', '28', 'bold'), text='7')
+        canvas.create_text(372,174, anchor=('se'), font=('Arial', '28', 'bold'), text='8')
+        canvas.create_text(448,174, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'),text='9')
+        canvas.create_text(524,174, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'), text='10')
+        canvas.create_text(68,235, anchor=('se'), font=('Arial', '28', 'bold'), text='11')
+        canvas.create_text(144,235, anchor=('se'), font=('Arial', '28', 'bold'), text='12')
+        canvas.create_text(220,235, anchor=('se'), font=('Arial', '28', 'bold'), text='13')
+        canvas.create_text(296,235, anchor=('se'), font=('Arial', '28', 'bold'), text='14')
+        canvas.create_text(372,235, anchor=('se'), font=('Arial', '28', 'bold'), text='15')
+        canvas.create_text(448,235, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'),text='16')
+        canvas.create_text(524,235, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'), text='17')
+        canvas.create_text(68,296, anchor=('se'), font=('Arial', '28', 'bold'), text='18')
+        canvas.create_text(144,296, anchor=('se'), font=('Arial', '28', 'bold'), text='19')
+        canvas.create_text(220,296, anchor=('se'), font=('Arial', '28', 'bold'), text='20')
+        canvas.create_text(296,296, anchor=('se'), font=('Arial', '28', 'bold'), text='21')
+        canvas.create_text(372,296, anchor=('se'), font=('Arial', '28', 'bold'), text='22')
+        canvas.create_text(448,296, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'), text='23')
+        canvas.create_text(524,296, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#FF6157'), text='24')
+        canvas.create_text(68,357, anchor=('se'), font=('Arial', '28', 'bold'), text='25')
+        canvas.create_text(144,357, anchor=('se'), font=('Arial', '28', 'bold'), text='26')
+        canvas.create_text(220,357, anchor=('se'), font=('Arial', '28', 'bold'), text='27')
+        canvas.create_text(296,357, anchor=('se'), font=('Arial', '28', 'bold'), text='28')
+        canvas.create_text(372,357, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='1')
+        canvas.create_text(448,357, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='2')
+        canvas.create_text(524,357, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='3')
+        canvas.create_text(68,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='4')
+        canvas.create_text(144,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='5')
+        canvas.create_text(220,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='6')
+        canvas.create_text(296,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='7')
+        canvas.create_text(372,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='8')
+        canvas.create_text(448,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='9')
+        canvas.create_text(524,418, anchor=('se'), font=('Arial', '28', 'bold'), fill=('#c2c2c2'), text='10')
 
         
         
